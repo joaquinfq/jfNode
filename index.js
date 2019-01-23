e6f8ca614e1c0ab82a4c476992f3935310508766
Joaquín Fernández <jf@joaquinfernandez.net>
2019-01-23 09:45:46 +0100

    Agregados los métodos dump, find, lookup y siblings

--- index.js
+++ index.js
@@ -9,33 +9,40 @@ class jfNode
     /**
      * Constructor de la clase.
      *
-     * @param {*}       data     Datos a almacenar en el nodo.
-     * @param {jf.Node} previous Nodo anterior al actual.
-     * @param {jf.Node} next     Nodo siguiente al actual.
+     * @param {object} config Configuración a aplicar a la instancia.
      */
-    constructor(data = null, previous = null, next = null)
+    constructor(config = {})
     {
         /**
-         * Datos almacenados en el nodo.
+         * Metadatos almacenados en el nodo.
          *
          * @property data
          * @type     {*}
          */
-        this.data = data;
+        this.data = null;
         /**
          * Nodo anterior.
          *
          * @property previous
          * @type     {jf.Node|null}
          */
-        this.previous = previous;
+        this.previous = null;
         /**
          * Nodo siguiente.
          *
          * @property next
          * @type     {jf.Node|null}
          */
-        this.next = next;
+        this.next = null;
+        /**
+         * Valor del nodo.
+         *
+         * @property value
+         * @type     {*}
+         */
+        this.value = null;
+        //------------------------------------------------------------------------------
+        this.setProperties(config);
     }
 
     /**
@@ -91,31 +98,112 @@ class jfNode
     /**
      * Clona el nodo actual y devuelve una copia.
      *
-     * @return {jf.jfNode} Clon del nodo.
+     * @return {jf.Node} Clon del nodo.
      */
     clone()
     {
         const _clone = new this.constructor();
-        Object.assign(_clone, this);
+        _clone.setProperties(this);
 
         return _clone;
     }
 
     /**
-     * Coloca en la lista de salida el valor del nodo.
+     * Realiza un volcado por pantalla del nodo para inspeccionaarlo.
+     */
+    dump()
+    {
+        console.log(JSON.stringify(this, null, 4));
+    }
+
+    /**
+     * Encuentra el nodo con el valor especificado permitiendo ir hacia adelante
+     * o hacia atrás.
+     *
+     * @param {string}  value    Valor a buscar.
+     * @param {string}  iterate  Propiedad del nodo sobre la que se iterará.
+     * @param {string}  property Propiedad que tiene el valor a buscar.
+     *
+     * @return {jf.Node|null} Nodo con el valor o `null` si no se encontró.
+     */
+    find(value, iterate = 'next', property = 'data')
+    {
+        let _node = this;
+        while (_node instanceof jfNode && _node[property] !== value)
+        {
+            _node = _node[iterate];
+        }
+
+        return _node;
+    }
+
+    /**
+     * Encuentra el primer node de la secuencia que no contiene la propiedad buscada o el
+     * valor de la propiedad no es un nodo.
+     *
+     * @param {string} property Propiedad que tiene el valor a buscar.
+     *
+     * @return {jf.Node} Primer nodo de la secuencia.
+     */
+    lookup(property = 'previous')
+    {
+        let _node = this;
+        while (_node instanceof jfNode)
+        {
+            if (property in _node && _node[property] instanceof jfNode)
+            {
+                _node = _node[property];
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return _node;
+    }
+
+    /**
+     * Coloca en la lista de salida el valor de una propiedad del nodo.
      *
      * AVISO: Este método es recursivo, si hay muchos nodos seguramente agote la memoria o
      * si ocurre un ciclo.
      *
-     * @param {array} output Lista de salida.
+     * @param {array}  output   Lista de salida.
+     * @param {string} property Nombre de la propiedad a extraer.
      */
-    concat(output)
+    pluck(output, property = 'data')
     {
-        output.push(this.data);
+        output.push(this[property]);
         const _next = this.next;
         if (_next)
         {
-            _next.concat(output);
+            _next.pluck(output);
+        }
+    }
+
+    /**
+     * Asigna los valores a las propiedades de la instancia.
+     *
+     * @param {object} values Valores a asignar.
+     */
+    setProperties(values)
+    {
+        if (values)
+        {
+            Object.keys(values).forEach(
+                property =>
+                {
+                    if (property in this)
+                    {
+                        const _value = values[property];
+                        if (_value !== undefined)
+                        {
+                            this[property] = _value;
+                        }
+                    }
+                }
+            );
         }
     }
 
@@ -136,6 +224,28 @@ class jfNode
             _next.previous = _previous;
             this.next      = null;
         }
+    }
+
+    /**
+     * Busca todos los nodos inmediatos con el valor especificado.
+     *
+     * @param {string} value    Valor a buscar.
+     * @param {string} iterate  Propiedad del nodo sobre la que se iterará.
+     * @param {string} property Propiedad que tiene el valor a buscar.
+     *
+     * @return {jf.Node[]} Listado de nodos encontrados.
+     */
+    siblings(value, iterate = 'next', property = 'data')
+    {
+        const _nodes = [];
+        let _node = this[iterate];
+        while (_node instanceof jfNode && _node[property] === value)
+        {
+            _nodes[iterate === 'next' ? 'push' : 'unshift'](_node);
+            _node = _node[iterate];
+        }
+
+        return _nodes;
     }
 
     /**
@@ -167,7 +277,15 @@ class jfNode
      */
     toJSON()
     {
-        return this.data;
+        const _data = {};
+        Object.keys(this)
+            .sort()
+            // Devolvemos todas las propiedades menos los 2 nodos y cualquiera que se haya agregado como privada.
+            .filter(property => property[0] !== '_' && property !== 'previous' && property !== 'next')
+            .forEach(property => _data[property] = this[property]);
+
+        return _data;
     }
 }
+
 module.exports = jfNode;
